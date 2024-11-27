6dd030a2feb33a7c17fc31f7fe27bd55b06617df
VaradM-17 <varadmule17@gmail.com>
2024-11-27 22:58:14 +0530

    service completed

--- src/main/java/com/jbk/Spring_Crud_HibernateMethods/service/EmployeeService.java
+++ src/main/java/com/jbk/Spring_Crud_HibernateMethods/service/EmployeeService.java
@@ -1,8 +1,42 @@
 package com.jbk.Spring_Crud_HibernateMethods.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jbk.Spring_Crud_HibernateMethods.dao.EmployeeDao;
+import com.jbk.Spring_Crud_HibernateMethods.entity.Employee;
 
 @Service
 public class EmployeeService {
+
+	@Autowired
+	EmployeeDao employeedao;
+
+	public String insertData(Employee e) {
+		String msg = employeedao.insertData(e);
+		return msg;
+	}
+
+	public String updateData(Employee e, int eid) {
+		String msg = employeedao.updateData(e, eid);
+		return msg;
+	}
+
+	public String deleteData(int eid) {
+		String msg = employeedao.deleteData(eid);
+		return msg;
+	}
+
+	public List<Employee> displayallData() {
+		List<Employee> list = employeedao.displayallData();
+		return list;
+	}
+
+	public Employee fetchsingleData(int eid) {
+		Employee e = employeedao.displaysingleData(eid);
+		return e;
+	}
 
 }
