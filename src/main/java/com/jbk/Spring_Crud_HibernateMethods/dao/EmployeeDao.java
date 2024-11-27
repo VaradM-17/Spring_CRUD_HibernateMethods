9570adc400c58c5fb2a1d52e448c991807b1205f
VaradM-17 <varadmule17@gmail.com>
2024-11-27 18:09:00 +0530

    dao commit

--- src/main/java/com/jbk/Spring_Crud_HibernateMethods/dao/EmployeeDao.java
+++ src/main/java/com/jbk/Spring_Crud_HibernateMethods/dao/EmployeeDao.java
@@ -1,8 +1,57 @@
 package com.jbk.Spring_Crud_HibernateMethods.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.jbk.Spring_Crud_HibernateMethods.entity.Employee;
 
 @Repository
 public class EmployeeDao {
+	@Autowired
+	SessionFactory factory;
+
+	public String insertData(Employee e) {
+
+		Session ss = factory.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		ss.persist(e);
+		tr.commit();
+		ss.close();
+
+		return "Data Inserted...";
+
+	}
+
+	public String updateData(Employee e, int eid) {
+		Session ss = factory.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		Employee emp = ss.get(Employee.class, eid);
+
+		e.setEname(e.getEname());
+		e.setEage(e.getEage());
+		e.setEsalary(e.getEsalary());
+
+		ss.merge(emp);
+		tr.commit();
+		ss.close();
+
+		return "Data Updated...";
+
+	}
+
+	public String deleteData(int eid) {
+		Session ss = factory.openSession();
+		Transaction tr = ss.beginTransaction();
+		
+		Employee e = ss.get(Employee.class, eid);
+		
+		return "Data Deleted...";
+
+	}
 
 }
