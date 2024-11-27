6fff7f636c96a5768b5b442a31f262924066090b
VaradM-17 <varadmule17@gmail.com>
2024-11-27 22:21:12 +0530

    dao completed

--- src/main/java/com/jbk/Spring_Crud_HibernateMethods/dao/EmployeeDao.java
+++ src/main/java/com/jbk/Spring_Crud_HibernateMethods/dao/EmployeeDao.java
@@ -1,12 +1,19 @@
 package com.jbk.Spring_Crud_HibernateMethods.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.jbk.Spring_Crud_HibernateMethods.entity.Employee;
+
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 @Repository
 public class EmployeeDao {
@@ -47,13 +54,32 @@ public class EmployeeDao {
 	public String deleteData(int eid) {
 		Session ss = factory.openSession();
 		Transaction tr = ss.beginTransaction();
-		
+
 		Employee e = ss.get(Employee.class, eid);
-		
+
 		ss.remove(e);
-		
+		tr.commit();
+		ss.close();
+
 		return "Data Deleted...";
 
+	}
+
+	public List<Employee> displayallData() {
+		Session ss = factory.openSession();
+
+		CriteriaQuery<Employee> cq = ss.getCriteriaBuilder().createQuery(Employee.class);
+
+		Root<Employee> root = cq.from(Employee.class);
+		cq.select(root);
+
+		Query<Employee> query = ss.createQuery(cq);
+
+		List<Employee> list = query.getResultList();
+
+		ss.close();
+
+		return list;
 	}
 
 }
